bb20e0f95ce3e3773c85528bb9987cd08d7c14a4
Mahendiran <Mahendiran@LAPTOP-PL02RL3S>
2018-05-21 16:59:39 +0530

    NO Change

--- src/CompleteInterview/Algorithams/SelectionSort.java
+++ src/CompleteInterview/Algorithams/SelectionSort.java
@@ -1,16 +1,31 @@
 package CompleteInterview.Algorithams;
 
+import java.util.Arrays;
+
 public class SelectionSort {
 
 	public static void main(String[] args) {
 		
-		int[] ins= {5,1,12,-5,16,2,12,14};
+		int[] ins= {9,14,3,2,43,11,58,22};
 		
-		int n=ins.length;
-		
-		while(n>0) {
-			
+		for (int i = 0; i < ins.length-1 ; i++) {
+			int min =ins[i];
+			int minindex=i;
+			for (int j2 = i+1; j2 < ins.length ; j2++) {
+				if(ins[j2]<min) {
+					min=ins[j2];
+					minindex=j2;
+				}
+			}
+			    if(ins[i]>ins[minindex]) {
+					int temp=ins[i];
+					ins[i]=ins[minindex];
+					ins[minindex]=temp;
+			    }
 		}
+		
+		System.out.println(Arrays.toString(ins));
+		
 
 	} 
 
