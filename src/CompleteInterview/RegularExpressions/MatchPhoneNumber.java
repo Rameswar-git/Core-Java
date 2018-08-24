5ed48cf8e09bd67de2a9653aca7ddcc50efeb953
fasupport <fasupport@FASL001.ind.rsa-ins.com>
2018-08-24 11:41:13 +0530

    Regular Expression added

---
+++ src/CompleteInterview/RegularExpressions/MatchPhoneNumber.java
@@ -0,0 +1,25 @@
+package CompleteInterview.RegularExpressions;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchPhoneNumber {
+
+	public static void main(String[] args) {
+		isValidPhone("+91 9238990601");
+	}
+
+	private static void isValidPhone(String str) {
+//		Pattern pat = Pattern.compile("^(\\+91[\\-])(\\d{10})");
+		
+//		Pattern pat = Pattern.compile("[789][\\d]{9}");
+		
+//		Pattern pat = Pattern.compile("^[0](\\d{3}[\\-]?)[\\d]{7}");
+		
+		Pattern pat = Pattern.compile("^(\\+91[-]?[\\s]?)(\\d{10})");
+		
+		Matcher match = pat.matcher(str);
+		System.out.println(match.matches());
+	}
+
+}
