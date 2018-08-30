fa323509c141b70489bcbcbbaf3c515fd57e3508
Rameswar-git <rameswarprasaddas@gmail.com>
2018-08-31 00:51:53 +0530

    sort alg change

--- src/CompleteInterview/Algorithams/BubbleSort.java
+++ src/CompleteInterview/Algorithams/BubbleSort.java
@@ -7,26 +7,18 @@ public class BubbleSort {
 	public static void main(String[] args) {
 
 		int[] nos = { 9, 3, 5, 8, 2, 6 };
-		
-            for (int i = 0; i < nos.length; i++) {
-            	for (int j = 0; j < nos.length-1; j++) {
-					int k=j+1;
-						if (nos[k] < nos[j]) {
-							int temp =nos[k];
-							nos[k] = nos[j];
-							nos[j] = temp;	
-						}
 
-					
-						
+		for (int i = 0; i < nos.length; i++) {
+			for (int j = 0; j < nos.length - 1; j++) {
+				if (nos[j + 1] < nos[j]) {
+					int temp = nos[j + 1];
+					nos[j + 1] = nos[j];
+					nos[j] = temp;
 				}
-				
+
 			}
 
-//				if (nos[count] > nos[k]) 
-//				int temp = nos[count];
-//				nos[count] = nos[k];
-//				nos[k] = temp;		
+		}
 		System.out.println(Arrays.toString(nos));
 
 	}
--- src/CompleteInterview/Algorithams/QuickSort.java
+++ src/CompleteInterview/Algorithams/QuickSort.java
@@ -5,37 +5,37 @@ import java.util.Arrays;
 public class QuickSort {
 
 	public static void main(String[] args) {
-		int[] arr = { 7,5,3,9,2,6,1,4 };
+		int[] arr = { 7, 5, 3, 9, 2, 6, 1, 4 };
 		Sort(arr, 0, arr.length - 1);
 		System.out.println(Arrays.toString(arr));
 
 	}
 
 	private static void Sort(int[] arr, int start, int end) {
-		if(start < end) {
-			int pp=Qs(arr,start,end);
-			Sort(arr,start,pp-1);
-			Sort(arr,pp+1,end);
+		if (start < end) {
+			int pp = Qs(arr, start, end);
+			Sort(arr, start, pp - 1);
+			Sort(arr, pp + 1, end);
 		}
 	}
 
 	private static int Qs(int[] arr, int start, int end) {
 		
-		int piviot=arr[end];
-		int i=start-1;
-		for (int j = start ; j < end ; j++) {
-			if(arr[j] <= piviot) {
+		int piviot = arr[end];
+		int i = start - 1;
+		for (int j = start; j < end; j++) {
+			if (arr[j] <= piviot) {
 				i++;
-				int temp=arr[j];
-				arr[j]=arr[i];
-				arr[i]=temp;
+				int temp = arr[j];
+				arr[j] = arr[i];
+				arr[i] = temp;
 			}
 		}
-		
-		int tmp2=arr[i+1];
-		arr[i+1]=piviot;
-		arr[end]=tmp2;
-		return i+1;
+
+		int tmp2 = arr[i + 1];
+		arr[i + 1] = piviot;
+		arr[end] = tmp2;
+		return i + 1;
 	}
 
 }
