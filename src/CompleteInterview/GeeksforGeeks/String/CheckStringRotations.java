c7e747eaf3c6c248607f5acebb577d4f496c56c9
Rameswar-git <rameswarprasaddas@gmail.com>
2018-09-02 01:39:27 +0530

    Changed

--- src/CompleteInterview/GeeksforGeeks/String/CheckStringRotations.java
+++ src/CompleteInterview/GeeksforGeeks/String/CheckStringRotations.java
@@ -2,39 +2,49 @@ package CompleteInterview.GeeksforGeeks.String;
 
 public class CheckStringRotations {
 
+//	public static void main(String[] args) {
+//
+//		String str1 = "amazon";
+//
+//		String str2 = "onamaz";
+//
+//		char c = 'a';
+//
+//
+//		for (int i = 0; i < str2.length(); i++) {
+//			if (str2.charAt(i) == c) {
+//				str2 =   str2.substring(i, str2.length())+str2.substring(0, i);
+//				break;
+//			}
+//		}
+//
+//		if (str1.equals(str2)) {
+//			System.out.println(" Matches in Clockwise ");
+//		}
+//
+//	}
+	
 	public static void main(String[] args) {
 
-		String str1 = "amazon";
+		String a = "ABCD";
+		String b = "CDAB";
 
-		String str2 = "onamaz";
-
-		char c = 'a';
-
-		// Anticlockwise checking
-
-		// for (int i = str2.length()-1; i >= 0 ; i--) {
-		// if(str2.charAt(i) == c) {
-		// str2=str2.substring(i, str2.length())+str2.substring(0, i);
-		// break;
-		// }
-		// }
-
-		// Clockwise checking
-
-		for (int i = 0; i < str2.length(); i++) {
-			if (str2.charAt(i) == c) {
-				str2 =   str2.substring(i, str2.length())+str2.substring(0, i);
-				break;
+		boolean test = false;
+		for (int i = 1; i < a.length(); i += 2) {
+			String sub = a.substring(i - 1, i + 1);
+			for (int j = sub.length() - 1, m = b.length() - i ; j >= 0 && m >= 0; j--, m--) {
+				if (!(b.charAt(m) == sub.charAt(j))) {
+					test = true;
+					break;
+				}
 			}
+
 		}
 
-		// if(str1.equals(str2)) {
-		// System.out.println(" Matches in Anti Clockwise ");
-		//
-		// }
-
-		if (str1.equals(str2)) {
-			System.out.println(" Matches in Clockwise ");
+		if (test) {
+			System.out.println(" The String is not rotated ");
+		} else {
+			System.out.println(" The Characters are rotated ");
 		}
 
 	}
--- src/CompleteInterview/GeeksforGeeks/String/LongestSubString.java
+++ src/CompleteInterview/GeeksforGeeks/String/LongestSubString.java
@@ -2,25 +2,58 @@ package CompleteInterview.GeeksforGeeks.String;
 
 public class LongestSubString {
 
+	// public static void main(String[] args) {
+	//
+	// String st1="abcd458963xyz";
+	// String st2="xyzab458963cd";
+	//
+	// String maxst="";
+	//
+	// for (int i = 1,k=st2.length(); i <= st2.length() && k>=0; i++,k--) {
+	//
+	// if(st1.contains(st2.substring(0,i)) && maxst.length() <
+	// st2.substring(0,i).length()) {
+	// maxst=st2.substring(0,i);
+	// }
+	// if(st1.contains(st2.substring(k,st2.length())) && maxst.length() <
+	// st2.substring(0,i).length()) {
+	// maxst=st2.substring(k,st2.length());
+	// }
+	//
+	// }
+	//
+	// System.out.println(" The Longest Common String between this two is "+maxst+"
+	// and its length is "+maxst.length());
+	//
+	// }
+
 	public static void main(String[] args) {
-		
-		String st1="abcd458963xyz";
-		String st2="xyzab458963cd";
-		
-		String maxst="";
-		
-		for (int i = 1,k=st2.length(); i <= st2.length() && k>=0; i++,k--) {
-			
-			if(st1.contains(st2.substring(0,i)) && maxst.length() < st2.substring(0,i).length()) {
-				maxst=st2.substring(0,i);	
+
+		String st1 = "abcd458963xyzlmrq";
+		String st2 = "xyzlmrqab458963cd";
+
+		StringBuilder build = new StringBuilder();
+		int k = 0;
+		outerloop: while (k < st1.length()) {
+			for (int j = k; j < st1.length(); j++) {
+				String substring = st1.substring(k, j + 1);
+				if (st2.indexOf(substring) >= 0) {
+					if (build.toString().length() < substring.length()) {
+						build.setLength(0);
+						build.append(substring);
+					} else if (k + substring.length() == st1.length()) {
+						break outerloop;
+					}
+
+				} else {
+					k = j;
+					break;
+				}
 			}
-			if(st1.contains(st2.substring(k,st2.length())) &&  maxst.length() < st2.substring(0,i).length()) {
-				maxst=st2.substring(k,st2.length());	
-			}
-			
+
 		}
-		
-		System.out.println(" The Longest Common String between this two is "+maxst+" and its length is "+maxst.length());
+
+		System.out.println(" The Longest Common Substring is " + build.toString());
 
 	}
 
