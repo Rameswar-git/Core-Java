f7acbd72fd4e9cb53449d674e06cb7bc9b39a690
Rameswar-git <rameswarprasaddas@gmail.com>
2018-09-03 00:20:59 +0530

    String Reverse Logic Changed

--- src/CompleteInterview/GeeksforGeeks/String/StringReverse.java
+++ src/CompleteInterview/GeeksforGeeks/String/StringReverse.java
@@ -1,36 +1,35 @@
 package CompleteInterview.GeeksforGeeks.String;
 
+import java.util.stream.IntStream;
 
 public class StringReverse {
-	
 
 	public static void main(String[] args) {
-		 String name="i.like.this.program.very.much";
-		 
-		 
-		 StringBuilder build=new StringBuilder();
-		 
-		 int itrcount=name.split("[.]").length;
+		String name = "i.like.this.program.very.much";
 
-		 
-		 for (int i = 0; i <  itrcount ; i++) {
-			 
-			 if(name.lastIndexOf(".") > 0) {
-				 build.append(name.substring(name.lastIndexOf("."), name.length())) ;
-				 name=name.substring(0, name.lastIndexOf("."));
-			 }else {
-				 build.append("."+name.substring(0, name.length())) ;
-			 }
+		StringBuilder build = new StringBuilder();
+		//
+		// int itrcount=name.split("[.]").length;
+		//
+		//
+		// for (int i = 0; i < itrcount ; i++) {
+		//
+		// if(name.lastIndexOf(".") > 0) {
+		// build.append(name.substring(name.lastIndexOf("."), name.length())) ;
+		// name=name.substring(0, name.lastIndexOf("."));
+		// }else {
+		// build.append("."+name.substring(0, name.length())) ;
+		// }
+		//
+		//
+		// }
 
-			
-		}
-
-           
-           System.out.println(build.toString());
+		char[] charcters = name.toCharArray();
 		
-		
+		IntStream.range(0, charcters.length).forEach(t -> build.append(charcters[charcters.length-t-1]));
+
+		System.out.println(build.toString());
 
 	}
-
 
 }
