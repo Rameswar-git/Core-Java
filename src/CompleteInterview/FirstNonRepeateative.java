89c9be9ce6bf922e6a76072b9a3530f4028204b5
fasupport <fasupport@FASL001.ind.rsa-ins.com>
2018-09-18 15:45:14 +0530

    String Rotation Logic and First Non repeatative character Logic got changed

--- src/CompleteInterview/FirstNonRepeateative.java
+++ src/CompleteInterview/FirstNonRepeateative.java
@@ -1,23 +1,37 @@
 package CompleteInterview;
 
+import java.util.Arrays;
 import java.util.BitSet;
+import java.util.Collections;
+import java.util.List;
 
 public class FirstNonRepeateative {
 
 	public static void main(String[] args) {
 		
-		String name="viccviky";
+//		String name="viccviky";
 			
 		
-		   BitSet seen=new BitSet(), repeated=new BitSet();
-		   name.chars().forEachOrdered(c -> (seen.get(c)? repeated: seen).set(c));
-		    if(repeated.isEmpty()) System.out.println("first unique: "+name.charAt(0));
-		    else {
-		    	name.chars().filter(repeated::get).findFirst()
-		                 .ifPresent(c -> System.out.println("first repeated: "+(char)c));
-		    	name.chars().filter(c -> !repeated.get(c)).findFirst()
-		                 .ifPresent(c -> System.out.println("first unique: "+(char)c));
-		    }
+//		   BitSet seen=new BitSet(), repeated=new BitSet();
+//		   name.chars().forEachOrdered(c -> (seen.get(c)? repeated: seen).set(c));
+//		    if(repeated.isEmpty()) System.out.println("first unique: "+name.charAt(0));
+//		    else {
+//		    	name.chars().filter(repeated::get).findFirst()
+//		                 .ifPresent(c -> System.out.println("first repeated: "+(char)c));
+//		    	name.chars().filter(c -> !repeated.get(c)).findFirst()
+//		                 .ifPresent(c -> System.out.println("first unique: "+(char)c));
+//		    }
+		
+		String name="geeksforgeeks";
+		
+		List<String> asList = Arrays.asList(name.split(""));
+		
+		
+		Character bs=name.chars().mapToObj(g -> (char)g)
+		      .filter(f -> Collections.frequency( asList , f.charValue()+"") == 1)
+		                  .findFirst().get();
+		
+		System.out.println(bs);
 	
 			
 	}
--- src/CompleteInterview/GeeksforGeeks/String/CheckStringRotations.java
+++ src/CompleteInterview/GeeksforGeeks/String/CheckStringRotations.java
@@ -24,28 +24,62 @@ public class CheckStringRotations {
 //
 //	}
 	
+//	public static void main(String[] args) {
+//
+//		String a = "ABCD";
+//		String b = "CDAB";
+//
+//		boolean test = false;
+//		for (int i = 1; i < a.length(); i += 2) {
+//			String sub = a.substring(i - 1, i + 1);
+//			for (int j = sub.length() - 1, m = b.length() - i ; j >= 0 && m >= 0; j--, m--) {
+//				if (!(b.charAt(m) == sub.charAt(j))) {
+//					test = true;
+//					break;
+//				}
+//			}
+//
+//		}
+//
+//		if (test) {
+//			System.out.println(" The String is not rotated ");
+//		} else {
+//			System.out.println(" The Characters are rotated ");
+//		}
+//
+//	}
+	
+
 	public static void main(String[] args) {
 
-		String a = "ABCD";
-		String b = "CDAB";
+		String name1 = "ABCD";
+		String name2 = "CBDA";
 
-		boolean test = false;
-		for (int i = 1; i < a.length(); i += 2) {
-			String sub = a.substring(i - 1, i + 1);
-			for (int j = sub.length() - 1, m = b.length() - i ; j >= 0 && m >= 0; j--, m--) {
-				if (!(b.charAt(m) == sub.charAt(j))) {
-					test = true;
-					break;
-				}
+		boolean startcheck = false;
+		boolean endcheck = false;
+		
+		boolean isrotated=false;
+
+		for (int i = 0, m = name1.length() - 1; i < name1.length() && m >= 0; i++, m--) {
+			String temp = name2.substring(0, i);
+			if (name2.endsWith(temp)) {
+				endcheck = true;
 			}
 
-		}
+			temp = name1.substring(m, name1.length());
 
-		if (test) {
-			System.out.println(" The String is not rotated ");
-		} else {
-			System.out.println(" The Characters are rotated ");
+			if (name2.startsWith(temp)) {
+				startcheck = true;
+			}
+			if(startcheck &&  endcheck){
+				isrotated=true;
+				break;
+				
+			}
+                 
 		}
+		
+		System.out.println(isrotated);
 
 	}
 
