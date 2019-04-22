bc6e5a12e8d97ad96935657417649d6b5e67e1c2
Rameswar-git <rameswarprasaddas@gmail.com>
2019-04-22 21:39:52 +0530

    Format

--- src/matrices_transpose/AllSubstrings.java
+++ src/matrices_transpose/AllSubstrings.java
@@ -3,13 +3,13 @@ package matrices_transpose;
 public class AllSubstrings {
 
 	public static void main(String[] args) {
-		String name="Vicky";
-  for(int i=0;i<=name.length();i++){
-	  for(int j=i;j<=name.length();j++){
-		  System.out.println(name.substring(i, j));
-	  }
-	  System.out.println();
-  }
+		String name = "Vicky";
+		for (int i = 0; i <= name.length(); i++) {
+			for (int j = i; j <= name.length(); j++) {
+				System.out.println(name.substring(i, j));
+			}
+			System.out.println();
+		}
 
 	}
 
