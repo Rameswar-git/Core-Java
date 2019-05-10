56690a23fad8fb3b4d4ffbab6ed5e983d75c2201
Rameswar <rameswarprasaddas@gmail.com>
2019-05-10 17:47:51 +0530

    Regular Expression Some Fixes

--- src/CompleteInterview/RegularExpression/MatchPhoneNumber.java
+++ src/CompleteInterview/RegularExpression/MatchPhoneNumber.java
@@ -16,10 +16,15 @@ public class MatchPhoneNumber {
 		
 //		Pattern pat = Pattern.compile("^[0](\\d{3}[\\-]?)[\\d]{7}");
 		
-		Pattern pat = Pattern.compile("^(\\+91[-]?[\\s]?)(\\d{10})");
-		
-		Matcher match = pat.matcher(str);
+		//Land Line No Check
+		Pattern landlineno = Pattern.compile("^(0\\d{3})([\\s+]|[-])(\\d{7})");
+		Matcher match = landlineno.matcher("0680-2221709");
 		System.out.println(match.matches());
+		
+//		Pattern pat = Pattern.compile("^(\\+91[-]?[\\s]?)(\\d{10})");
+//		
+//		Matcher match = pat.matcher(str);
+//		System.out.println(match.matches());
 	}
 
 }
