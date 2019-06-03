1cf6ed458f26b159d87793bf7a98148172e80b4a
Rameswar-git <rameswarprasaddas@gmail.com>
2019-06-03 23:55:18 +0530

    Angular 6+ different Enviroment Configs added

---
+++ src/HackerRank/NewYearChaos.java
@@ -0,0 +1,46 @@
+package HackerRank;
+
+import java.util.Scanner;
+//https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=arrays
+public class NewYearChaos {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int testCases = input.nextInt();
+		while (testCases > 0) {
+			testCases--;
+			int n = input.nextInt();
+			int[] inputLine = new int[n];
+			for (int a = 0; a < n; a++) {
+				inputLine[a] = input.nextInt();
+			}
+			int[] outputFrequencyDistribution = new int[n + 1];
+			boolean chaos = false;
+			boolean finished = false;
+			int count = 0;
+			while (!finished) {
+				finished = true;
+				for (int a = 0; a < n - 1; a++) {
+					if (inputLine[a] > inputLine[a + 1]) {
+						outputFrequencyDistribution[inputLine[a]]++;
+						if (outputFrequencyDistribution[inputLine[a]] > 2) {
+							finished = true;
+							chaos = true; // if it's too chaotic, break out
+							break;
+						}
+						count++;
+						int temp = inputLine[a];
+						inputLine[a] = inputLine[a + 1];
+						inputLine[a + 1] = temp;
+						finished = false;
+					}
+				}
+			}
+			if (chaos) {
+				System.out.println("Too chaotic");
+			} else {
+				System.out.println(count);
+			}
+		}
+	}
+}
+
