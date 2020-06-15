038fea927a3d3a2f94aa9fd9b2abf950220d2d57
Rameswar-git <rameswarprasaddas@gmail.com>
2020-06-15 21:42:50 +0530

    new Branch for hacker rank added

---
+++ src/com/rameswar/hackerRank/PS/QueensAttack.java
@@ -0,0 +1,9 @@
+package com.rameswar.hackerRank.PS;
+
+public class QueensAttack {
+
+	public static void main(String[] args) {
+		System.out.println("Hacker rank branch and package added");
+	}
+
+}
