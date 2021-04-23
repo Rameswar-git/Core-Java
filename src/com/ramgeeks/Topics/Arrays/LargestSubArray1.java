55eaf0aaffc6c79c25c4a9c9e7218423fff9f3c8
C5323492 <C5323492@c02drqv0md6m>
2021-04-23 19:34:49 +0530

    last comit added some probleme statment

--- src/com/ramgeeks/Topics/Arrays/LargestSubArray1.java
+++ src/com/ramgeeks/Topics/Arrays/LargestSubArray1.java
@@ -2,6 +2,23 @@ package com.ramgeeks.Topics.Arrays;
 
 import java.util.HashMap;
 
+/*
+ * Given an array of 0s and 1s. Find the length of the largest subarray with equal number of 0s and 1s.
+
+Example 1:
+
+Input:
+N = 4
+A[] = {0,1,0,1}
+Output: 4
+Explanation: The array from index [0...3]
+contains equal number of 0's and 1's.
+Thus maximum length of subarray having
+equal number of 0's and 1's is 4.
+ * 
+ * 
+ */
+
 public class LargestSubArray1 {
 
 	// Returns largest subarray with
@@ -63,7 +80,8 @@ public class LargestSubArray1 {
 		int arr[] = { 1, 0, 0, 1, 0, 1, 1 };
 		int n = arr.length;
 
-		System.out.println(sub.maxLen(arr, n));;
+		System.out.println(sub.maxLen(arr, n));
+		;
 	}
 
 }
